62917d54af556eaeaf7286dfd384665d9cccb098
Matheus Scarpato Fidelis <msfidelis01@gmail.com>
2021-06-30 16:54:02 -0300

    Simple Refactor and Consumer Go Routines Improvments

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/google/uuid v1.2.0 // indirect
-	github.com/segmentio/kafka-go v0.4.16 // indirect
+	github.com/segmentio/kafka-go v0.4.16
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/tools v0.1.4 // indirect
 )
---
+++ pkg/kafkadialer/main.go
@@ -0,0 +1,35 @@
+package kafkadialer
+
+import (
+	"crypto/tls"
+	"os"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func GetDialer(ssl bool) kafka.Dialer {
+	var dialer kafka.Dialer
+	name, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if ssl {
+		dialer = kafka.Dialer{
+			Timeout:   20 * time.Second,
+			DualStack: true,
+			ClientID:  name,
+			TLS:       &tls.Config{},
+		}
+	} else {
+		dialer = kafka.Dialer{
+			Timeout:   20 * time.Second,
+			DualStack: true,
+			ClientID:  name,
+		}
+	}
+
+	return dialer
+}
